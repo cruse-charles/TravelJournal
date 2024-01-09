7b17afed7f3a475a4342902b8611eeea57873dc6
Charles Cruse <charle9@vt.edu>
2024-01-08 23:05:18 -0500

    added credential validation and jwt cookie for login controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
@@ -33,12 +34,12 @@ export const login = async (req, res, next) => {
         const isPasswordValid = bcryptjs.compareSync(password, user.password);
         if (!isPasswordValid) return next(errorHandler(401, 'Invalid credentials'));
         
-        res.status(200).json('User logged in');
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+        
+        const {password: pass, ...userWithoutPassword} = user._doc;
+
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(userWithoutPassword);
     } catch (error) {
         next(error);
     }
-
-
-
-
 };
