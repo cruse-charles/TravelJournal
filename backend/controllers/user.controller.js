567f8fca1cf5a3bed98cc169754e68fa152f5b3c
Charles Cruse <charle9@vt.edu>
2024-01-19 12:40:28 +0800

    added comments to user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,13 +2,17 @@ import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 
 export const updateUser = async (req, res, next) => {
+    // Check if user is updating their own data
     if (req.user.id !== req.params.id) return next(errorHandler(403, 'You can only update your own user data'));
     
     try {
         if(req.body.password) {
+            // Hash the password before saving it to the database
             req.body.password = bcryptjs.hashSync(req.body.password, 10);
         }
 
+        // The MongoDB $set operator replaces the value of a field with the specified value
+        // {new: true} option returns the modified document rather than the original
         const updatedUser = await User.findByIdAndUpdate(req.params.id, {
             $set:{
                 username: req.body.username,
@@ -17,6 +21,7 @@ export const updateUser = async (req, res, next) => {
             }
         }, {new: true});
 
+        // Deconstruct the password field from the updated user document and send the rest of the data
         const {password, ...rest} = updatedUser._doc;
         res.status(200).json(rest);
     } catch (error) {
