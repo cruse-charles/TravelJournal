3c17847f77e7436ed1b1d44cd71817cc55de6ac2
Charles Cruse <charle9@vt.edu>
2024-02-28 16:42:05 +0800

    displayed all users pages on profile page

--- frontend/src/components/UserPages.jsx
+++ frontend/src/components/UserPages.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
 const UserPages = () => {
+    const { currentUser } = useSelector(state => state.user)
+    const [pages, setPages] = useState([])
+
+    useEffect(() => {
+
+        const controller = new AbortController();
+
+        axios.get(`api/user/pages/${currentUser._id}`, { signal: controller.signal })
+            .then(res => {
+                setPages(res.data)
+            }).catch((error) => {
+                console.log(error.response.data.message)
+            })
+
+        return () => controller.abort();
+    }, [])
+
+
     return (
-        <div>UserPages</div>
+        <>
+            <div>UserPages</div>
+            {pages.map(page => (
+                <div>
+                    <div>{page.title}</div>
+                    <div>{page.text}</div>
+                    <div>{page._id}</div>
+                    {page.attachments?.map((imageURL, index) => {
+                        return <img key={index} src={imageURL} />
+                    })}
+                </div>
+            ))}
+        </>
     )
 }
 
