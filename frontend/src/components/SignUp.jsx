4c08d9caa1a2c569f811fcee474c5d38e3fa52dd
Charles Cruse <charle9@vt.edu>
2024-03-29 22:58:03 -0400

    added submission to signup api

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 const SignUp = () => {
+    const [formValues, setFormValues] = useState({})
+
+    const handleChange = (e) => {
+        const { id, value } = e.target
+        setFormValues({ ...formValues, [id]: value })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await axios.post('api/auth/signup', formValues)
+            .then(res => {
+                console.log(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
     return (
         <>
             <div>SignUp</div>
-            <form>
-                <input type="text" id="username" placeholder="Username" />
-                <input type="email" id="email" placeholder="Email" />
-                <input type="password" id="password" placeholder="Password" />
+            <form onSubmit={handleSubmit}>
+                <input type="text" id="username" placeholder="Username" onChange={handleChange} />
+                <input type="email" id="email" placeholder="Email" onChange={handleChange} />
+                <input type="password" id="password" placeholder="Password" onChange={handleChange} />
                 <button>Sign Up</button>
             </form>
         </>
