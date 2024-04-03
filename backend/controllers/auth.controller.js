a7366ee99a33afa9f0da0a2d5565765ed10d1329
Charles Cruse <charle9@vt.edu>
2024-04-03 01:44:11 -0400

    created signup api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,17 @@
 import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
 
 export const signup = async (req, res) => {
-    // const {username, email, password} = req.body;
-    res.send('Signup route');
+    const {username, email, password} = req.body;
+
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    const newUser = new User({username, email, password: hashedPassword});
+    await newUser.save()
+        .then(() => {
+        res.status(201).json('User created');
+        })
+        .catch((error) => {
+            res.status(400).json({message: error.message});
+        });
 };
