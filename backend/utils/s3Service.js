9791fe96dfb8f6d78515b33af6229bbf15b0973d
Charles Cruse <charle9@vt.edu>
2024-04-16 23:37:05 -0400

    add comments to S3service file

--- backend/utils/s3Service.js
+++ backend/utils/s3Service.js
@@ -1,12 +1,10 @@
-import { randomImageName } from "../utils/s3ImageNameHelper.js";
+import { randomImageName } from "./s3ImageNameHelper.js";
 import { resizeImage } from "./s3ImageResizer.js";
 
 import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-// import sharp from 'sharp';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 const s3BucketName = process.env.S3_BUCKET_NAME
@@ -14,6 +12,7 @@ const s3BucketRegion = process.env.S3_BUCKET_REGION
 const s3AccessKey = process.env.S3_ACCESS_KEY
 const s3SecretAccessKey = process.env.S3_SECRET_ACCESS_KEY
 
+// Create a new S3 client
 const s3 = new S3Client({
     credentials: {
         accessKeyId: s3AccessKey,
@@ -22,7 +21,10 @@ const s3 = new S3Client({
     region: s3BucketRegion
 })
 
+// Get signed URLs for images in S3 for a page
 export const getImageURLsFromS3 = async (pageAttachments) => {
+
+    // Create an array of signed URLs for each image
     const urlPromises = pageAttachments.map(async (attachment) => {
         const getObjectParams = {
             Bucket: s3BucketName,
@@ -32,12 +34,15 @@ export const getImageURLsFromS3 = async (pageAttachments) => {
         const command = new GetObjectCommand(getObjectParams);
         return getSignedUrl(s3, command, { expiresIn: 3600 });
     })
+    // await all signed URL promises
     return Promise.all(urlPromises);
 }
 
+// Save images to S3 bucket and return image names
 export const saveImagesToS3 = async (files) => {
     const attachments = [];
 
+    // Resize each image and give random name and upload to S3
     const uploadPromises = files.map(async (file) => {
         const buffer = await resizeImage(file.buffer);
         const imageName = randomImageName();
@@ -49,11 +54,12 @@ export const saveImagesToS3 = async (files) => {
             Body: buffer,
             ContentType: file.mimetype
         };
-    
+
         const command = new PutObjectCommand(params);
         return s3.send(command);
     });
-    
+
+    // await all image upload promises and return array of image names
     await Promise.all(uploadPromises);
     return attachments;
 }
