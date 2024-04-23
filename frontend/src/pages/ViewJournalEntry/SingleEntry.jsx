f857474a44aebdca277bfed4eecf775bb8c90fc4
Charles Cruse <charle9@vt.edu>
2024-04-23 16:00:59 +0800

    carousel matches size of outside component

--- frontend/src/pages/ViewJournalEntry/SingleEntry.jsx
+++ frontend/src/pages/ViewJournalEntry/SingleEntry.jsx
@@ -158,58 +158,27 @@ const SingleEntry = () => {
             ) : (
                 <>
                     <form onSubmit={handleSubmit}>
-                        <Flex >
-                            {/* <Stack style={{ width: '60%' }}>
-                                <Dropzone accept={IMAGE_MIME_TYPE} onDrop={handleImageChange} style={{ width: '100%', height: '50%' }}>
-                                    <Text ta="center">Drop images here</Text>
-                                </Dropzone>
-                                <SimpleGrid cols={{ base: 1, sm: 4 }} mt={previews.length > 0 ? 'xl' : 0}>
-                                    {previews.map((item, index) => {
-                                        const isFile = item instanceof File;
-                                        const src = isFile ? URL.createObjectURL(item) : item;
-                                        const key = isFile ? `file-${item.name}` : `url-${item}`;
-                                        return (
-                                            <Indicator key={key} size={15} color="blue" offset={-2} onClick={() => deleteSelectedImage(key)}>
-                                                <Image key={key} src={src} onLoad={() => URL.revokeObjectURL(src)} />
+                        <Flex style={{ height: '80vh' }}>
+                            <Carousel style={{ width: '100%' }} height='100%' loop withIndicators slideSize={{ base: '100%' }}>
+                                {previews.map((item, index) => {
+                                    const isFile = item instanceof File;
+                                    const src = isFile ? URL.createObjectURL(item) : item;
+                                    const key = isFile ? `file-${item.name}` : `url-${item}`;
+                                    return (
+                                        <Carousel.Slide key={key} >
+                                            <Indicator key={key} size={15} color="blue" offset={12} onClick={() => deleteSelectedImage(key)}>
+                                                <Image key={key} src={src} onLoad={() => URL.revokeObjectURL(src)} style={{ fit: 'contain' }} />
                                             </Indicator>
-                                        )
-                                    })}
-                                </SimpleGrid>
-                            </Stack> */}
-                            <Stack style={{ width: '60%' }}>
-                                <Carousel style={{ width: '100%' }} height='100%' loop withIndicators slideSize={{ base: '100%' }}>
-                                    {previews.map((item, index) => {
-                                        const isFile = item instanceof File;
-                                        const src = isFile ? URL.createObjectURL(item) : item;
-                                        const key = isFile ? `file-${item.name}` : `url-${item}`;
-                                        return (
-                                            <Carousel.Slide key={key} >
-                                                <Indicator key={key} size={15} color="blue" offset={12} onClick={() => deleteSelectedImage(key)}>
-                                                    <Image key={key} src={src} onLoad={() => URL.revokeObjectURL(src)} />
-                                                </Indicator>
-                                            </Carousel.Slide>
-                                        )
-                                    })}
-                                    <Carousel.Slide>
-                                        <Dropzone accept={IMAGE_MIME_TYPE} onDrop={handleImageChange} style={{ width: '100%', height: '100%' }}>
-                                            <Text ta="center">Drop images here</Text>
-                                        </Dropzone>
-                                    </Carousel.Slide>
-                                </Carousel>
-                                {/* <SimpleGrid cols={{ base: 1, sm: 4 }} mt={previews.length > 0 ? 'xl' : 0}>
-                                    {previews.map((item, index) => {
-                                        const isFile = item instanceof File;
-                                        const src = isFile ? URL.createObjectURL(item) : item;
-                                        const key = isFile ? `file-${item.name}` : `url-${item}`;
-                                        return (
-                                            <Indicator key={key} size={15} color="blue" offset={-2} onClick={() => deleteSelectedImage(key)}>
-                                                <Image key={key} src={src} onLoad={() => URL.revokeObjectURL(src)} />
-                                            </Indicator>
-                                        )
-                                    })}
-                                </SimpleGrid> */}
-                            </Stack>
-                            <Stack style={{ width: '40%' }} gap='xs'>
+                                        </Carousel.Slide>
+                                    )
+                                })}
+                                <Carousel.Slide>
+                                    <Dropzone accept={IMAGE_MIME_TYPE} onDrop={handleImageChange} style={{ width: '100%', height: '100%' }}>
+                                        <Text ta="center">Drop images here</Text>
+                                    </Dropzone>
+                                </Carousel.Slide>
+                            </Carousel>
+                            <Stack style={{ width: '30%', padding: '0px 10px', height: '100%' }}>
                                 <Group>
                                     <TextInput onChange={handleChange} placeholder='Title of your day!' name='title' radius="xs" size='lg' style={{ width: '80%' }} value={entry.title} maxLength={40} />
                                     <Button type='submit' color="black">Save</Button>
