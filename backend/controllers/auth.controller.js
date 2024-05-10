da4083675234754a3873707adef6c8a69453631a
Charles Cruse <charle9@vt.edu>
2024-05-09 22:20:47 -0400

    created custom error handler

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -12,6 +12,6 @@ export const signup = async (req, res) => {
         res.status(201).json('User created');
         })
         .catch((error) => {
-            res.status(400).json({message: error.message});
+            next(error);
         });
 };
--- backend/server.js
+++ backend/server.js
@@ -34,5 +34,9 @@ app.use((error, req, res, next) => {
     const statusCode = error.statusCode || 500;
     const message = error.message || 'Internal Server Error';
 
-    return res.status(statusCode).json({statusCode, message});
+    return res.status(statusCode).json({
+        success: false, 
+        statusCode, 
+        message
+    });
 })
