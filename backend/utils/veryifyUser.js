aa791db4e8e8e9624db7a2087aba33e5d3d014f3
Charles Cruse <charle9@vt.edu>
2024-05-20 12:26:30 +0800

    added comments to verifyUser middleware

--- backend/utils/veryifyUser.js
+++ backend/utils/veryifyUser.js
@@ -2,17 +2,20 @@ import { errorHandler } from "./error.js";
 import jwt from 'jsonwebtoken';
 
 export const verifyToken = (req, res, next) => {
+    // Extract the access token from the cookies sent in the request
     const token = req.cookies.access_token;
 
     if (!token) {
         return next(errorHandler(401, 'Unauthorized'));
     }
 
+    // Verify the token using the secret key
     jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
         if (error) {
             return next(errorHandler(401, 'Unauthorized'));
         }
 
+        // If the token is valid, attach the user payload to the request object
         req.user = user;
         next();
     });
