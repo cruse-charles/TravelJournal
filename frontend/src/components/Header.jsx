96c0288ec6d78d6f6d5b669a49f179c1d381436f
Charles Cruse <charle9@vt.edu>
2024-05-29 16:13:05 +0800

    added text and styling to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='bg-slate-200 shadow-md'>
+      <div className='flex justify-between items-center mas-w-6xl mx-auto p-3'>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+          <span className="text-slate-500">Travel Journal</span>
+        </h1>
+        <ul className='flex gap-4'>
+          <li className='text-slate-700 hover:underline' >Home</li>
+          <li className='text-slate-700 hover:underline'>About</li>
+          <li className='text-slate-700 hover:underline'>Sign in</li>
+        </ul>
+      </div>
+    </header >
   )
 }
 
