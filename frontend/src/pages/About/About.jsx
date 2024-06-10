b13d88d0dc7c6ea2dbc5911038fcff7353155afa
Charles Cruse <charle9@vt.edu>
2024-06-11 00:04:45 +0800

    edited alignment of text and added sign up button

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -22,18 +22,19 @@ const About = () => {
                 <Stack align='center' gap='xs' style={{ width: '66%' }}>
                     <Title order={1}>About TravelJournal</Title>
                     <Text align='center' size='xl'>Travel Journal is a platform designed to help you capture and share your travel experiences. Whether you're an avid explorer or a casual traveler, our app provides a beautiful and personal way to document your journeys, preserve your memories, and inspire others.</Text>
-                    <Group>
-                        <Stack align='center' style={{ width: '30%' }}>
+                    <Group align='flex-start'>
+                        <Stack justify='flex-start' align='center' style={{ width: '30%' }}>
                             <Title align='center'>About the Developer</Title>
                             <Text align='center'>Travel Journal was created by Charles, a passionate adventurer and software engineer. With a deep love for exploring new places and capturing their essence, Charles built this platform to help others document their own travel experiences.</Text>
                         </Stack>
-                        <Stack align='center' style={{ width: '30%' }}>
-                            <Title align='center'>Built with...</Title>
+                        <Stack justify='flex-start' align='center' style={{ width: '30%' }}>
+                            <Title align='center'>Built with Latest Technologies</Title>
                             <Text align='center'>Travel Journal is powered by React and Express, popular for building fast and scalable web applications. We use MongoDB for our database and AWS for secure image uploads, ensuring a seamless and reliable experience for our users.</Text>
                         </Stack>
-                        <Stack align='center' style={{ width: '30%' }}>
+                        <Stack justify='flex-start' align='center' style={{ width: '30%' }}>
                             <Title align='center'>Start Journaling Your Adventures</Title>
                             <Text align='center'>Sign up today and begin capturing your travel memories through journal entries, photos, and more. Travel Journal is the perfect companion for your adventures, helping you preserve and share your unique experiences with the world.</Text>
+                            <Button color='black'>Sign Up</Button>
                         </Stack>
                     </Group>
                 </Stack>
