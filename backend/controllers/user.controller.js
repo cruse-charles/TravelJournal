4c03996de799f884eefc5c648e956d2b2ea432dc
Charles Cruse <charle9@vt.edu>
2024-06-12 16:58:29 +0800

    added delete user route and deleteUser function to user.controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -29,3 +29,15 @@ export const updateUser = async (req, res, next) => {
         next(error)
     }
 }
+
+export const deleteUser = async (req, res, next) => {
+    // Check if user is deleting their own data
+    if (req.user.id !== req.params.id) return next(errorHandler(403, 'You can only delete your own user data'));
+    
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json('User has been deleted')
+    } catch (error) {
+        next(error)
+    }
+}
