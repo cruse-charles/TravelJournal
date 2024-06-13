b50ef0cc2589f09adfc894fa1add4e65d865b49c
Charles Cruse <charle9@vt.edu>
2024-06-13 11:49:24 +0800

    added comments to Calendar component

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -9,15 +9,21 @@ const Calendar = () => {
     const datepickerRef = useRef(null);
 
     useEffect(() => {
-        new AirDatepicker(datepickerRef.current, {
+        // Create date-picker calendar instance
+        const datePicker = new AirDatepicker(datepickerRef.current, {
+            // Set language to English
             locale: en,
+
+            // Callback function to analyze each cell of calendar
             onRenderCell({ date, cellType }) {
                 let dates = [1, 5, 7, 10, 15, 20, 25],
+                    // check if cell is a day cell and extract day
                     isDay = cellType === 'day',
                     _date = date.getDate(),
                     shouldChangeContent = isDay && dates.includes(_date);
 
                 return {
+                    // Change content of cell if it is a day cell and date is in dates array
                     html: shouldChangeContent ? `${_date}<div class="dot"></div>` : undefined,
                     classes: shouldChangeContent ? '-dot-cell-' : undefined,
                     attrs: {
@@ -27,6 +33,10 @@ const Calendar = () => {
             }
         })
 
+        // Clean up function to destroy date-picker instance
+        return () => {
+            datePicker.destroy();
+        }
     }, [])
 
 
