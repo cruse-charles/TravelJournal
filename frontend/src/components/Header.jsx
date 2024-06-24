8eb5613f278334432ca03973270aa5973d4c5b26
Charles Cruse <charle9@vt.edu>
2024-06-24 10:11:11 +0800

    updated header and buttons style

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,34 +1,66 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux';
+import { Flex, Button } from '@mantine/core';
 
 const Header = () => {
 
   const { currentUser } = useSelector(state => state.user);
 
+  // return (
+  //   <Flex mih={50}
+  //     gap="md"
+  //     justify="flex-end"
+  //     align="center"
+  //     direction="row"
+  //     wrap="wrap">
+  //     <div className=''>
+  //       <Link to='/'>
+  //         <div className=''>
+  //           <span className="">Travel Journal</span>
+  //         </div>
+  //       </Link>
+  //       <ul className=''>
+  //         <Link to='/'>
+  //           <li className='' >Home</li>
+  //         </Link>
+  //         <Link to='/about'>
+  //           <li className=''>About</li>
+  //         </Link>
+  //         <Link to='/profile'>
+  //           {currentUser ? (<div>{currentUser.username}</div>) : (
+  //             <li className=''>Sign in</li>
+  //           )}
+  //         </Link>
+  //       </ul>
+  //     </div>
+  //   </Flex>
+  // )
+
   return (
-    <header className=''>
+    <Flex mih={50}
+      gap="md"
+      justify="flex-end"
+      align="center"
+      direction="row"
+      wrap="wrap">
       <div className=''>
-        <Link to='/'>
-          <h1 className=''>
-            <span className="">Travel Journal</span>
-          </h1>
-        </Link>
-        <ul className=''>
-          <Link to='/'>
-            <li className='' >Home</li>
-          </Link>
-          <Link to='/about'>
-            <li className=''>About</li>
-          </Link>
-          <Link to='/profile'>
-            {currentUser ? (<div>{currentUser.username}</div>) : (
-              <li className=''>Sign in</li>
-            )}
-          </Link>
-        </ul>
+        <Button component={Link} to="/">
+          <div className="">Travel Journal</div>
+        </Button>
+        <Button component={Link} to="">
+          <div className='' >Home</div>
+        </Button>
+        <Button component={Link} to="/about">
+          <div className=''>About</div>
+        </Button>
+        <Button component={Link} to="/profile">
+          {currentUser ? (<div>{currentUser.username}</div>) : (
+            <div className=''>Sign in</div>
+          )}
+        </Button>
       </div>
-    </header >
+    </Flex>
   )
 }
 
