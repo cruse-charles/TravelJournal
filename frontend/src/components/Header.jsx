889ac1c8322677e58ea87c8a18b41da556eddd5b
Charles Cruse <charle9@vt.edu>
2024-06-25 10:35:01 +0800

    aligned header and buttons to correct positions

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,65 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux';
-import { Flex, Button } from '@mantine/core';
+import { Flex, Button, Container } from '@mantine/core';
 
 const Header = () => {
 
   const { currentUser } = useSelector(state => state.user);
 
-  // return (
-  //   <Flex mih={50}
-  //     gap="md"
-  //     justify="flex-end"
-  //     align="center"
-  //     direction="row"
-  //     wrap="wrap">
-  //     <div className=''>
-  //       <Link to='/'>
-  //         <div className=''>
-  //           <span className="">Travel Journal</span>
-  //         </div>
-  //       </Link>
-  //       <ul className=''>
-  //         <Link to='/'>
-  //           <li className='' >Home</li>
-  //         </Link>
-  //         <Link to='/about'>
-  //           <li className=''>About</li>
-  //         </Link>
-  //         <Link to='/profile'>
-  //           {currentUser ? (<div>{currentUser.username}</div>) : (
-  //             <li className=''>Sign in</li>
-  //           )}
-  //         </Link>
-  //       </ul>
-  //     </div>
-  //   </Flex>
-  // )
-
   return (
-    <Flex mih={50}
-      gap="md"
-      justify="flex-end"
+    <Flex
       align="center"
       direction="row"
-      wrap="wrap">
-      <div className=''>
+      wrap="wrap"
+    >
+      <Container style={{ width: '55%' }} align='left'>
         <Button component={Link} to="/">
-          <div className="">Travel Journal</div>
+          <div>Travel Journal</div>
         </Button>
+      </Container>
+      <Container style={{ width: '35%' }} align='right'>
         <Button component={Link} to="">
-          <div className='' >Home</div>
+          <div >Home</div>
         </Button>
         <Button component={Link} to="/about">
-          <div className=''>About</div>
+          <div>About</div>
         </Button>
         <Button component={Link} to="/profile">
           {currentUser ? (<div>{currentUser.username}</div>) : (
-            <div className=''>Sign in</div>
+            <div>Sign in</div>
           )}
         </Button>
-      </div>
+      </Container>
     </Flex>
   )
 }
