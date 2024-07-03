4049ded68d6a5d1190055e42f7e999c4cdab937b
Charles Cruse <charle9@vt.edu>
2024-07-03 16:41:04 +0800

    added login and redirect to profile page after signup

--- frontend/src/pages/Signup/SignUp.jsx
+++ frontend/src/pages/Signup/SignUp.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../../redux/user/userSlice';
 
 import { NavLink, Text, Button, TextInput, Space, Paper, PasswordInput, Center } from '@mantine/core';
 
@@ -10,6 +13,11 @@ const SignUp = () => {
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(false)
 
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+
+
     const handleChange = (e) => {
         const { id, value } = e.target
         setFormValues({ ...formValues, [id]: value })
@@ -18,16 +26,45 @@ const SignUp = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         setLoading(true);
-        await axios.post('api/auth/signup', formValues)
-            .then(res => {
-                setLoading(false);
-                setError(null);
-            })
-            .catch(err => {
-                console.log(err.response.data);
-                setError(err.response.data.message);
-                setLoading(false);
-            })
+
+        try {
+            await axios.post('api/auth/signup', formValues);
+            setError(null);
+
+            // If signup is successful, proceed to login
+            const loginResponse = await axios.post('api/auth/login', formValues);
+            dispatch(signInSuccess(loginResponse.data));
+            navigate('/profile');
+        } catch (err) {
+            console.log(err.response?.data);
+            setError(err.response?.data?.message);
+        } finally {
+            setLoading(false);
+        }
+
+
+        // await axios.post('api/auth/signup', formValues)
+        //     .then(res => {
+        //         setLoading(false);
+        //         setError(null);
+        //         const loginResponse = await axios.post('api/auth/login', formValues)
+        //         dispatch(signInSuccess(loginResponse.data));
+        //     })
+        //     .catch(err => {
+        //         console.log(err.response.data);
+        //         setError(err.response.data.message);
+        //         setLoading(false);
+        //     })
+
+        // await axios.post('api/auth/login', formValues)
+        //     // set errors and loading state
+        //     .then(res => {
+        //         dispatch(signInSuccess(res.data));
+        //         navigate('/profile')
+        //     })
+        //     .catch(err => {
+        //         dispatch(signInFailure(err.response.data.message));
+        //     })
     }
 
     return (
