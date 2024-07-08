6a2b55dd0f825e4a383069338e567726def773a0
Charles Cruse <charle9@vt.edu>
2024-07-08 15:19:10 +0800

    redirected to journalpage viewpage from card in profile page

--- frontend/src/components/UserPages.jsx
+++ frontend/src/components/UserPages.jsx
@@ -3,10 +3,12 @@ import axios from 'axios';
 import { useSelector } from 'react-redux';
 import { Card, Image, Text } from '@mantine/core';
 import { Carousel } from '@mantine/carousel';
+import { useNavigate } from 'react-router-dom';
 
 
 const UserPages = () => {
     const { currentUser } = useSelector(state => state.user)
+    const navigate = useNavigate();
     const [pages, setPages] = useState([])
 
     useEffect(() => {
@@ -23,12 +25,16 @@ const UserPages = () => {
         return () => controller.abort();
     }, [])
 
+    const handleNavigate = (id) => {
+        navigate(`/page/${id}`)
+    }
+
 
     return (
         <>
             {pages.map((page) => (
                 <Carousel.Slide key={page._id}>
-                    <Card key={page._id} shadow="sm" padding="lg" radius="md" withBorder style={{ width: 650, height: 500 }}>
+                    <Card onClick={() => handleNavigate(page._id)} key={page._id} shadow="sm" padding="lg" radius="md" withBorder style={{ width: 650, height: 500 }}>
                         <Card.Section>
                             <Image h={300} src={page.attachments?.[0]} />
                         </Card.Section>
