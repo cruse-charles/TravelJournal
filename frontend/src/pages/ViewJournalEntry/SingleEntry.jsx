c798aa4a1a7d14e9a21b377b171f2f1842a14845
Charles Cruse <charle9@vt.edu>
2024-07-18 14:19:24 +0800

    added editing toggle state

--- frontend/src/pages/ViewJournalEntry/SingleEntry.jsx
+++ frontend/src/pages/ViewJournalEntry/SingleEntry.jsx
@@ -16,6 +16,7 @@ const SingleEntry = () => {
     // state vars for entry and loading
     const [entry, setEntry] = useState({});
     const [isLoading, setIsLoading] = useState(true);
+    const [isEditing, setIsEditing] = useState(false)
 
     // useDisclosure hook to open and close modal
     const [opened, { open, close }] = useDisclosure(false);
@@ -66,37 +67,47 @@ const SingleEntry = () => {
         return {};
     }
 
+    const startEdit = () => {
+        setIsEditing(true)
+    }
+
     return (
         <>
-            <Flex align="flex-start">
-                <Carousel style={{ width: '75%', height: '560px' }} loop withIndicators>
-                    {entry?.attachments?.map((imageURL, index) => {
-                        return (
-                            <Carousel.Slide key={index} style={{ width: '75%', height: '560px' }} >
-                                <Image key={index} src={imageURL} />
-                            </Carousel.Slide>
-                        )
-                    })}
-                </Carousel>
-                <Stack style={{ width: '25%' }}>
-                    <Group>
-                        <Title order={2} style={{ width: '60%' }}>{entry?.title}</Title>
-                        <Group gap='xs'>
-                            <Button variant="outline" color="gray" size="xs"><FaPencil /></Button>
-                            <Button onClick={handleDelete} variant="outline" color="gray" size="xs"><FaRegTrashCan /></Button>
-                        </Group>
-                    </Group>
-                    <ScrollArea h={500}>
-                        <Text>{entry?.text}</Text>
-                    </ScrollArea>
-                </Stack>
-            </Flex>
-            <Modal opened={opened} onClose={close} title="Select a Date" size='auto'>
-                <DatePicker
-                    getDayProps={getDayProps}
-                />
-            </Modal>
-            <Button onClick={open}><FaCalendarDay />View Calendar</Button>
+            {!isEditing ? (
+                <>
+                    <Flex align="flex-start">
+                        <Carousel style={{ width: '75%', height: '560px' }} loop withIndicators>
+                            {entry?.attachments?.map((imageURL, index) => {
+                                return (
+                                    <Carousel.Slide key={index} style={{ width: '75%', height: '560px' }} >
+                                        <Image key={index} src={imageURL} />
+                                    </Carousel.Slide>
+                                )
+                            })}
+                        </Carousel>
+                        <Stack style={{ width: '25%' }}>
+                            <Group>
+                                <Title order={2} style={{ width: '60%' }}>{entry?.title}</Title>
+                                <Group gap='xs'>
+                                    <Button variant="outline" color="gray" size="xs" onClick={startEdit}><FaPencil /></Button>
+                                    <Button onClick={handleDelete} variant="outline" color="gray" size="xs"><FaRegTrashCan /></Button>
+                                </Group>
+                            </Group>
+                            <ScrollArea h={500}>
+                                <Text>{entry?.text}</Text>
+                            </ScrollArea>
+                        </Stack>
+                    </Flex>
+                    <Modal opened={opened} onClose={close} title="Select a Date" size='auto'>
+                        <DatePicker
+                            getDayProps={getDayProps}
+                        />
+                    </Modal>
+                    <Button onClick={open}><FaCalendarDay />View Calendar</Button>
+                </>
+            ) : (
+                <div>Hello</div>
+            )}
         </>
     )
 }
