7005d37eb79ebeea391229f2edc8e87b723c4238
Charles Cruse <charle9@vt.edu>
2024-08-09 11:54:02 +0800

    added menu dropdown for login/logout/signup

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux';
-import { Flex, Button, Container, Text } from '@mantine/core';
+import { Flex, Button, Container, Text, Menu } from '@mantine/core';
 
 const Header = () => {
 
   const { currentUser } = useSelector(state => state.user);
+  const navigate = useNavigate();
+
+  const handleProfileClick = () => {
+    navigate('/profile')
+  }
 
   return (
     <Flex
@@ -15,21 +20,48 @@ const Header = () => {
       style={{ backgroundColor: 'black', height: '100%', padding: '0px' }}
     >
       <Container style={{ width: '55%' }} align='left' >
-        <Button component={Link} to="/" color="black">
-          <Text>TravelJournal</Text>
-        </Button>
+        <Button component={Link} to="/" color="black">TravelJournal</Button>
       </Container>
       <Container style={{ width: '35%' }} align='right'>
-        <Button component={Link} to="" color="black">
-          <Text >Home</Text>
-        </Button>
-        <Button component={Link} to="/profile" color="black">
-          {currentUser ? (<Text>{currentUser.username}</Text>) : (
-            <Text>Login</Text>
+        <Button component={Link} to="" color="black">Home</Button>
+
+
+
+
+
+        <Menu>
+          <Menu.Target>
+            {currentUser ? (<Button color="black">{currentUser.username}</Button>) : (<Button onClick={handleProfileClick} color='black'>Login/Sign Up</Button>)}
+          </Menu.Target>
+          {currentUser ? (
+            <Menu.Dropdown>
+              <Menu.Item onClick={handleProfileClick}>
+                Profile
+              </Menu.Item>
+              <Menu.Item>
+                Logout
+              </Menu.Item>
+              <Menu.Divider />
+              <Menu.Label>
+                Danger zone
+              </Menu.Label>
+              <Menu.Item color='red'>
+                Delete Account
+              </Menu.Item>
+            </Menu.Dropdown>
+          ) : (
+            <Menu.Dropdown>
+              <Menu.Item onClick={handleProfileClick}>
+                Login
+              </Menu.Item>
+              <Menu.Item>
+                Sign Up
+              </Menu.Item>
+            </Menu.Dropdown>
           )}
-        </Button>
+        </Menu>
       </Container>
-    </Flex>
+    </Flex >
   )
 }
 
