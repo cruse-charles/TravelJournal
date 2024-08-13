865cfcd7e9c55e86ee0f286890f6eae7ac6a04d8
Charles Cruse <charle9@vt.edu>
2024-08-13 13:18:01 +0800

    cleaned up styling for carousel slides

--- frontend/src/pages/Profile/UserEntries.tsx
+++ frontend/src/pages/Profile/UserEntries.tsx
@@ -5,6 +5,7 @@ import { Card, Image, Text } from '@mantine/core';
 import { Carousel } from '@mantine/carousel';
 import { useNavigate } from 'react-router-dom';
 import Autoplay from 'embla-carousel-autoplay';
+import styles from './UserEntries.module.css';
 
 type Entry = {
     _id: string;
@@ -59,7 +60,7 @@ const UserEntries: React.FC = () => {
     return (
         <Carousel slideGap="md" loop dragFree plugins={[autoplay.current]} onMouseEnter={autoplay.current.stop} onMouseLeave={autoplay.current.reset}>
             {entries.map((entry) => (
-                <Carousel.Slide key={entry._id} className='Carousel-slide' style={{ height: '100%', width: '100%' }}>
+                <Carousel.Slide key={entry._id} className={styles.carouselSlide} >
                     <Card onClick={() => handleNavigate(entry._id)} key={entry._id} shadow="sm" padding="lg" radius="md" withBorder >
                         <Card.Section>
                             <Image style={{ width: '100%', height: '50%' }} h={350} src={entry.attachments?.[0]} />
