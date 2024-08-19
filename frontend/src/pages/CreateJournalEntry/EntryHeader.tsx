915c24cfb46b3e6342c83268a0ac72b10485acc5
Charles Cruse <charle9@vt.edu>
2024-08-20 00:35:47 +0800

    code cleanup

--- frontend/src/pages/CreateJournalEntry/EntryHeader.tsx
+++ frontend/src/pages/CreateJournalEntry/EntryHeader.tsx
@@ -48,8 +48,6 @@ const EntryHeader = ({error, formValues, isSaving, isEditing, handleChange, star
     }
 
     return (
-        <>
-            { isEditing ? 
             <>
                 <Group justify='space-between'>
                     <Group>
@@ -58,34 +56,12 @@ const EntryHeader = ({error, formValues, isSaving, isEditing, handleChange, star
                         <Title order={3}>{formValues?.date ? format(formValues?.date, 'MMMM do, yyy') : ''}</Title>
                         {error.date && <Text color='red'>{error.date}</Text>}
                     </Group>
-                    {/* <Button type='submit' disabled={isSaving} color="black">Save</Button> */}
                     {renderButtons()}
                 </Group>
                 <Center>
-                    {/* <TextInput value={formValues.title} onChange={handleChange} error={error.title} placeholder='Title of your day!' name='title' radius="xs" size='lg' className={styles.entryTitle} maxLength={40} /> */}
                     {renderTitleOrInput()}
                 </Center>
             </> 
-            : 
-            <>
-                <Group justify='space-between'>
-                    <Group>
-                            <Title order={3}>{formValues?.date ? format(formValues.date, 'MMMM do, yyy') : ''}</Title>
-                            <Button color="black" onClick={open} size='sm' variant='outline' style={{ border: 'none' }}><FaCalendarDay /></Button>
-                        </Group>
-                        {/* <Group>
-                            <Button variant="outline" color="black" size="xs" onClick={startEdit}><FaPencil /></Button>
-                            <Button onClick={handleDelete} variant="outline" color="black" size="xs"><FaRegTrashCan /></Button>
-                        </Group> */}
-                        {renderButtons()}
-                    </Group>
-                    <Center>
-                        {/* <Title order={1}>{formValues?.title}</Title> */}
-                        {renderTitleOrInput()}
-                    </Center>
-            </>
-            }
-        </>
     )
 }
 
