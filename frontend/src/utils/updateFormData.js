399ef7edc24355bc1cce8cd0c5dab99d6b119cea
Charles Cruse <charle9@vt.edu>
2024-08-22 14:11:50 +0800

    added comments to updatedForm Data

--- frontend/src/utils/updateFormData.js
+++ frontend/src/utils/updateFormData.js
@@ -1,11 +1,14 @@
 // Update form data with new files and entry data
 export const updateFormData = (entry, updatedFiles) => {
+    //create FormData object to submit object with files
     const formData = new FormData()
 
+    // append data and files to FormData object
     updatedFiles.forEach((file, index) => {
         formData.append(`attachments`, file);
     })
 
+    //TODO: THIS HERE IS ACTUALLY DOING 3 ATTACHMENT KEYS WITH EACH FILE IN ONE, CHANGE TO MAKE IT AN ARRAY UNDER ONE KEY
     Object.keys(entry).forEach(key => {
         if (key !== 'attachments') {
             formData.append(key, entry[key]);
