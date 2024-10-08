bc75b6e5230c2b98e5645fc90893d5a0e4a842cd
Charles Cruse <charle9@vt.edu>
2024-10-09 00:40:28 +0800

    adjusted text and title props

--- frontend/src/pages/About/About.tsx
+++ frontend/src/pages/About/About.tsx
@@ -53,20 +53,20 @@ const About = () => {
             <Flex justify='center' align='center'>
                 <Stack align='center' gap='xs' className={styles.secondSectionContainer}>
                     <Title order={1}>About TravelJournal</Title>
-                    <Text align='center' size='xl'>Travel Journal is a platform designed to help you capture and share your travel experiences. Whether you're an avid explorer or a casual traveler, our app provides a beautiful and personal way to document your journeys, preserve your memories, and inspire others.</Text>
+                    <Text ta='center' size='xl'>Travel Journal is a platform designed to help you capture and share your travel experiences. Whether you're an avid explorer or a casual traveler, our app provides a beautiful and personal way to document your journeys, preserve your memories, and inspire others.</Text>
                     <Space h='xl' />
                     <Group align='flex-start'>
                         <Stack justify='flex-start' align='center' className={styles.secondSectionText}>
-                            <Title align='center'>About the Developer</Title>
-                            <Text align='center'>Travel Journal was created by Charles Cruse, a passionate adventurer and software engineer. With a deep love for exploring new places and capturing their essence, Charles built this platform to help others document their own travel experiences.</Text>
+                            <Title ta='center'>About the Developer</Title>
+                            <Text ta='center'>Travel Journal was created by Charles Cruse, a passionate adventurer and software engineer. With a deep love for exploring new places and capturing their essence, Charles built this platform to help others document their own travel experiences.</Text>
                         </Stack>
                         <Stack justify='flex-start' align='center' className={styles.secondSectionText}>
-                            <Title align='center'>Built with Leading Technologies</Title>
-                            <Text align='center'>Travel Journal is powered by React and Express, popular for building fast and scalable web applications. We use MongoDB for our database and AWS for secure image uploads, ensuring a seamless and reliable experience for our users.</Text>
+                            <Title ta='center'>Built with Leading Technologies</Title>
+                            <Text ta='center'>Travel Journal is powered by React and Express, popular for building fast and scalable web applications. We use MongoDB for our database and AWS for secure image uploads, ensuring a seamless and reliable experience for our users.</Text>
                         </Stack>
                         <Stack justify='flex-start' align='center' className={styles.secondSectionText}>
-                            <Title align='center'>Start Journaling Your Adventures</Title>
-                            <Text align='center'>Sign up today and begin capturing your travel memories through journal entries, photos, and more. Travel Journal is the perfect companion for your adventures, helping you preserve and share your unique experiences with the world.</Text>
+                            <Title ta='center'>Start Journaling Your Adventures</Title>
+                            <Text ta='center'>Sign up today and begin capturing your travel memories through journal entries, photos, and more. Travel Journal is the perfect companion for your adventures, helping you preserve and share your unique experiences with the world.</Text>
                             <Button color='black' onClick={handleSignupButtonClick}>Sign Up</Button>
                         </Stack>
                     </Group>
